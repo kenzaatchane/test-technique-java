079d863345f78169431cff8d3902ba4192ba8ccf
ATCHANE Kenza <atchanek@pglw0210.lild01.pictime.fr>
2022-03-30 11:34:59 +0200

    fix the delete button

--- src/main/java/adeo/leroymerlin/cdp/EventRepository.java
+++ src/main/java/adeo/leroymerlin/cdp/EventRepository.java
@@ -5,7 +5,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Transactional(readOnly = true)
+@Transactional
 public interface EventRepository extends Repository<Event, Long> {
 
     void delete(Long eventId);
